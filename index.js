a49a3077874d11a6c7e924751a7ce26dd65d63d4
EdgarMTdev <edgarmontiel961@gmail.com>
2022-08-26 12:02:14 -0500

    delete: monolith app

--- index.js
+++
@@ -1,9 +0,0 @@
-const app = require("./src/app");
-const { port } = require("./src/config");
-
-const PORT = port || 4000
-
-
-app.listen(PORT, () => {
-    console.log('Running in http://localhost:' + port)
-})
