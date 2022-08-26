f585f84f0006c552099bf8d67a23ce4bc11407b8
EdgarMTdev <edgarmontiel961@gmail.com>
2022-08-26 12:01:00 -0500

    add: structure to microservice of users

---
+++ microservices/users/index.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const { port } = require('./src/config')
+
+const app = express()
+
+app.listen(port, () => {
+    console.log('Running in: http://localhost:' + port)
+})
