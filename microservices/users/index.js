45d22f77d8765dd4a749838c79bb4e6ed3c7152b
EdgarMTdev <edgarmontiel961@gmail.com>
2022-08-29 09:22:41 -0500

    refactor: functions to register new users

--- microservices/users/index.js
+++ microservices/users/index.js
@@ -1,11 +1,18 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const { port } = require('./src/config')
 const users = require('./src/routes')
 
 const app = express()
 app.use(express.json())
 app.use(morgan('dev'))
+
+const corsOptions = {
+    origin: ['http://localhost:4000']
+}
+
+app.use(cors(corsOptions))
 
 users(app)
 
--- microservices/users/src/service/index.js
+++ microservices/users/src/service/index.js
@@ -1,16 +1,17 @@
 const prisma = require("../libs/db");
+const { Prisma } = require("@prisma/client");
 
 class User {
     async getAll() {
         try {
-            const users = await prisma.user.findMany()
+            const users = await prisma.user.findMany();
             console.log(users);
             return {
                 success: true,
-                users
-            }
+                users,
+            };
         } catch (error) {
-            return error
+            return error;
         }
     }
 
@@ -18,32 +19,38 @@ class User {
         try {
             const result = prisma.user.findUnique({
                 where: {
-                    email
-                }
+                    email,
+                },
             });
             return {
                 success: true,
-                result
-            }
+                result,
+            };
         } catch (error) {
-            return error
+            return error;
         }
     }
 
     async create(data) {
         try {
-            const user = await prisma.user.create({ data })
+            const user = await prisma.user.create({ data });
             return {
                 success: true,
-                user
-            }
+                user,
+            };
         } catch (error) {
-            return {
-                success: false,
-                error: error.message
+            if (error instanceof Prisma.PrismaClientKnownRequestError) {
+                if (error.code === "P2002") {
+                    return {
+                        success: false,
+                        message:
+                            "There is a unique constraint violation, a new user cannot be created with this " +
+                            error.meta.target[0],
+                    };
+                }
             }
         }
     }
 }
 
-module.exports = User
+module.exports = User;
